c405870cdca3952dc5e6c1cc9ea15a25887b936b
arun <arunkumar12712@gmail.com>
2023-01-21 13:16:06 +0530

    section 12 completed

--- Abby.Web/Pages/Admin/Order/OrderDetails.cshtml.cs
+++ Abby.Web/Pages/Admin/Order/OrderDetails.cshtml.cs
@@ -1,8 +1,11 @@
 using Abby.DataAccess.Repository;
 using Abby.DataAccess.Repository.IRepository;
+using Abby.Models;
 using Abby.Models.ViewModel;
+using Abby.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Stripe;
 
 namespace Abby.Web.Pages.Admin.Order
 {
@@ -22,6 +25,39 @@ namespace Abby.Web.Pages.Admin.Order
                 OrderHeader= _unitOfWork.OrderHeader.GetFirstOrDefault(x => x.Id == id, includeProperties:"ApplicationUser"),
                 OrderDetails = _unitOfWork.OrderDetail.GetAll(x => x.OrderId == id).ToList()
             };
+        }
+
+        public IActionResult OnPostOrderCompleted(int orderId)
+        {
+            _unitOfWork.OrderHeader.UpdateStatus(orderId, SD.StatusCompleted);
+            _unitOfWork.Save();
+            return RedirectToPage("OrderList");
+        }
+
+        public IActionResult OnPostOrderRefund(int orderId)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(o => o.Id == orderId);
+
+            var options = new RefundCreateOptions
+            {
+                Reason = RefundReasons.RequestedByCustomer,
+                PaymentIntent = orderHeader.PaymentIntentId,
+                Amount = (long)orderHeader.OrderTotal
+            };
+
+            var service = new RefundService();
+            Refund refund = service.Create(options);
+
+            _unitOfWork.OrderHeader.UpdateStatus(orderId, SD.StatusRefunded);
+            _unitOfWork.Save();
+            return RedirectToPage("OrderList");
+        }
+
+        public IActionResult OnPostOrderCancel(int orderId)
+        {
+            _unitOfWork.OrderHeader.UpdateStatus(orderId, SD.StatusCancelled);
+            _unitOfWork.Save();
+            return RedirectToPage("OrderList");
         }
     }
 }
--- Abby.Web/wwwroot/js/orderList.js
+++ Abby.Web/wwwroot/js/orderList.js
@@ -1,7 +1,31 @@
 ﻿$(document).ready(function () {
-  dataTable =  $('#DT_Load').DataTable({
+    debugger
+    var url = window.location.search;
+    if (url.includes("cancelled")) {
+        loadList("cancelled");
+    }
+    else {
+        if (url.includes("completed")) {
+            loadList("completed");
+        }
+        else {
+            if (url.includes("ready")) {
+                loadList("ready");
+            }
+            else {
+                loadList("inprocess");
+            }
+        }
+    }
+
+    //alert('call');
+});
+
+function loadList(param) {
+   
+    dataTable = $('#DT_Load').DataTable({
         "ajax": {
-            "url": "/api/Order",
+            "url": "/api/Order?status=" + param,
             "type": "GET",
             "datatype": "json"
         },
@@ -17,47 +41,10 @@
                     return `<div>
                                 <a href="OrderDetails?id=${data}" class="btn btn-success text-white"
                                 style="cursor:pointer;width:100px;"><i class="bi-pencil-square"></i></a>
-                               
                             </div>`
                 }
             }
         ],
-        "width":"100%"
-
+        "width": "100%"
     });
-});
-
-function Delete(url) {
-    Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            $.ajax({
-                url: url,
-                type: 'DELETE',
-                success: function (data) {
-                    if (data.success) {
-                        dataTable.ajax.reload();
-                        toastr.success(data.message);
-                    }
-                    else {
-                        toastr.error(data.message);
-                    }
-                }
-            });
-
-            //Swal.fire(
-            //    'Deleted!',
-            //    'Your file has been deleted.',
-            //    'success'
-            //)
-        }
-    });
-
 }
